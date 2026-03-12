d43764b1ae9cbfae24e75021f28e1960e8da8036
MariusAKM <104445327+MariusAKM@users.noreply.github.com>
2026-03-12 17:27:25 +0100

    Update page.tsx

--- app/t/[tag]/page.tsx
+++ app/t/[tag]/page.tsx
@@ -1,1 +1,13 @@
-
+export default async function TagPage({
+  params,
+}: {
+  params: { tag: string };
+}) {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>MINDR</h1>
+      <p>Tag scanned:</p>
+      <p>{params.tag}</p>
+    </main>
+  );
+}
